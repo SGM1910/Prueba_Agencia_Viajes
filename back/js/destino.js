6f2e1d00c6d322d2d047eceaee069c6b02ff6f37
Saihara <sgmaranetto@gmail.com>
2025-05-16 16:11:15 -0300

    base de datos conectda a front. revisar error

--- back/js/destino.js
+++ back/js/destino.js
@@ -3,7 +3,7 @@ async function obtenerDestinos() {
     try {
         //todo lo q esta aca adentro si hay un error no se congela, salta a donde dice catch
     
-        const respuesta = await fetch("../backend/routes/api.php?seccion=destino"); //este valor no puede ser modificado, por eso constante y no variable
+        const respuesta = await fetch("../back/API/routes/API.php?seccion=destinos"); //este valor no puede ser modificado, por eso constante y no variable
    
         const destinos = await respuesta.json(); //convierte la respuesta q le pdio al seridor a un json
 console.log(destinos)
@@ -20,10 +20,11 @@ console.log(destinos)
 //mostrar los destinos en formato de texto
 function mostrarDestinos(destinos) {
     let contenido = "";
+    
 console.log(destinos);
     destinos.forEach(destinos => {
        
-        contenido += `<img src="https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2024/01/ilustracion-viajes-turismo-3259609.jpg?tf=3840x"`;
+        contenido += `<img src="https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2024/01/ilustracion-viajes-turismo-3259609.jpg?tf=3840x">`;
         contenido += `<div class="card-content">`;
         contenido += `<h3 class="card-title">${destinos.nombre_destino}</h3>`;
         contenido += `<p class="card-text">${destinos.descripcion_destino}</p>`;
